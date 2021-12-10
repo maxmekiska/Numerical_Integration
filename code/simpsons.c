c5a34fb0ab2c8acd793d2866d4b1e6d0fcee6b5b
maxmekiska <maxmekiska@gmail.com>
2021-12-10 21:36:37 +0000

    added docstrings

--- code/simpsons.c
+++ code/simpsons.c
@@ -1,5 +1,20 @@
 #include "numericintegration.h"
 
+/*
+ * Function:  simpsons
+ * -----------------------
+ * numerical integration via the simpson's rule algorithm:
+ * 
+ * Tn = delta x * 1/3 * (f(x0) + 4f(x1) + 2f(x2) + 4f(x3) + 2f(x4) + ... + 2f(xn-2) + 4f(xn-1) + f(xn))
+ * 
+ *  delta (double)      : length of subintervals = (upper/lower)/ subintervals n
+ *  upper (float)       : upper integral limit
+ *  lower (float)	: lower integral limit
+ *  (*f)(float) (float) : function to be numerically integrated
+ *
+ *  returns: numerical evaluation of integral.
+ */
+
 float simpsons(float delta, float upper, float lower, float (*f)(float)) {
   float interval = lower;
   float deltax;
